a09289106eefa1dee6dcca04f1b080e08a91d9b4
Hoàng <hoang9cnd@gmail.com>
2018-09-20 20:43:50 +0700

    week1

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,5 +1,3 @@
-package week1;
-
 public class Student{
 
     // TODO: khai báo các thuộc tính cho Student
@@ -30,20 +28,12 @@ public class Student{
 		this.Email = email;
 	}
 	
-<<<<<<< HEAD
     Student() {
     	this.Name = "Student";
     	this.Id = "000";
     	this.Group = "INT22041";
     	this.Email = "uet@vnu.edu.vn";
-=======
-    public Student() {
-    	this.name = "Student";
-    	this.id = "000";
-    	this.group = "INT22041";
-    	this.email = "uet@vnu.edu.vn";
->>>>>>> 3faae443a8441fb8b019137c6d7618b216ea4485
-    }
+		}
 
     /**
      * Constructor 2
@@ -51,45 +41,26 @@ public class Student{
      * @param sid
      * @param em
      */
-<<<<<<< HEAD
+
     Student(String n, String sid, String em) {
         this.Name = n;
         this.Id = sid;
 		this.Group = "INT22041";
         this.Email = em;
-=======
-    public Student(String n, String sid, String em) {
-        this.name = n;
-        this.id = sid;
-        this.email = em;
->>>>>>> 3faae443a8441fb8b019137c6d7618b216ea4485
-    }
+	}
 
     /**
      * Constructor 3
      * @param s
      */
-<<<<<<< HEAD
     Student(Student s) {
         this.Name  = s.Name;
         this.Id = s.Id;
         this.Group = s.Group;
         this.Email = s.Email;
     }
-
-    String getInfo() {
-        String s = this.Name + " " + this.Id + " " + this.Group +" " + this.Email;
-=======
-    public Student(Student s) {
-        this.name  = s.name;
-        this.id = s.id;
-        this.group = s.group;
-        this.email = s.email;
-    }
-
     public String getInfo() {
-        String s = name + " " + id + " " + group +" " +email;
->>>>>>> 3faae443a8441fb8b019137c6d7618b216ea4485
+        String s = this.Name + " " + this.Id + " " + this.Group +" " +this.Email;
        return s;
     }
 }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,6 +1,5 @@
-package week1;
+
 import java.util.HashSet;
-import java.util.ArrayList;
 //2
 public class StudentManagement {
 //11
@@ -15,14 +14,14 @@ public class StudentManagement {
     } 
 //12
     public void studentsByGroup() {
-	    HashSet<String> StudentGroupList = new HashSet<String>();
+	    HashSet<String> StudentGroupList = new HashSet<String>(); // List lưu Group
 	    for(int i = 0; i < Number; i++) {
 	    	StudentGroupList.add(students[i].getGroup());
 	    }
 	    for(String Group : StudentGroupList) {
 	    	System.out.println("Class "+ Group +":");
 	    	for(int i = 0; i < Number; i++) {
-	    		if(students[i].getGroup().equals(Group)) {
+	    		if(students[i].getGroup().equals(Group)) { 
 	    			System.out.println(students[i].getInfo());
 		}
 			}
@@ -31,7 +30,7 @@ public class StudentManagement {
 
 //13
     public void removeStudent(String id) {
-		int Index = -999;
+		int Index = -999; // tạo 1 chỉ mục đến students cần xóa
 		for(int i = 0; i < Number;i ++)
 		{
 			if(students[i].getId().equals(id))
@@ -43,10 +42,12 @@ public class StudentManagement {
 		if(Index != -999)
 		{
 			students[Index] = null;
-			for(int i = 0; i < Number - 1; i++)
+			for(int i = Index; i < Number - 1; i++)
 			{
 				students[i] = students[i+1];
 			}
+			Number--;
+			System.out.print("Complete!");
 		}
 		else System.out.println("Can't Not Delete That Student By Id");
 	}
@@ -62,18 +63,25 @@ public class StudentManagement {
 //9
 		Student st2 = new Student();
 		st2.setName("Vũ Hoàng Việt");
-		st2.setId("17020788");
+		st2.setId("17020778");
 		st2.setGroup("INT22042");
 		st2.setEmail("hoang92cnd@gmail.com");
 //8
 		Student st3 = new Student("Hoàng Vũ Việt","17026575","17020778@vnu.edu.vn");
 		Student st4 = new Student(st2);
+		Student st5 = new Student();
+		st1.setName("Hoàng");
+		st1.setId("17020788");
+		st1.setGroup("INT22043");
+		st1.setEmail("hoang934cnd@gmail.com");
+		System.out.println(st1.getInfo());
 		StudentManagement StudentList = new StudentManagement();
 		StudentList.students[0]=st1;
 		StudentList.students[1]=st2;
 		StudentList.students[2]=st3;
 		StudentList.students[3]=st4;
-		StudentList.removeStudent("17021567");
+		StudentList.removeStudent("17020778");
+				StudentList.removeStudent("17020788");
 		StudentList.studentsByGroup();
 //10
 		StudentManagement student = new StudentManagement();
