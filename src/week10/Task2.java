94ce7a9b9da8edb8d4d267b4d6a83152e67e7564
Kuro1s <hoang9cnd@gmail.com>
2018-11-14 14:07:06 +0700

    update

--- src/week10/Task2.java
+++ src/week10/Task2.java
@@ -4,21 +4,30 @@ import java.util.Scanner;
 
 public class Task2 {
     /**
+     * phương thức sinh số thực ngẫu nhiên
+     * @return  một mảng số thực 1000 phần tử
+     */
+    public double[] inputArray() {
+        double[] ramdom = new double[1000];
+        for (int i = 0; i < 1000; i++) {
+            ramdom[i] = Math.random() * 1000 ;
+        }
+        return ramdom;
+    }
+
+    /**
      * Thuận toán sắp xếp Bubble Sort
+     *
      * @param a là mảng cần sắp xếp
      * @return một mảng sap khi sắp xếp thep Bubble Sort
      */
-    public double[] BubbleSort(double a[])
-    {
-        for(int i = 0; i < a.length; i++)
-        {
-            for(int j = 0; j < a.length - i - 1; j++)
-            {
-                if(a[j] > a[j+1])
-                {
+    public double[] BubbleSort(double a[]) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
                     double temp = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = temp;
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
                 }
             }
         }
@@ -26,17 +35,10 @@ public class Task2 {
     }
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); 
-        int N = input.nextInt();
-        double[] a = new double[N];
-        for (int i = 0; i < a.length; i++)
-        {
-            a[i] = input.nextDouble();
-        }
         Task2 b = new Task2();
+        double[] a = b.inputArray();
         b.BubbleSort(a);
-        for(double st:a)
-        {
+        for (double st : a) {
             System.out.println(st);
         }
     }
--- src/week9/Utils.java
+++ src/week9/Utils.java
@@ -1,94 +1,97 @@
 package week9;
-
+import javax.rmi.CORBA.Util;
 import java.io.*;
 
 public class Utils {
     /**
-     * phương thức đọc vào xâu ký tự từ file
+     * phương thức đọc vào 1 xâu ký tự từ file
      * @param path  đường dẫn tới thư mục chưa file cần đọc
-     * @return   xâu ký tự là nội dung của file
+     * @return  một xâu ký tự là nội dung của file
      */
-    public static String readContentFormFile(String path) {
+    public static String readContentFormFile(String path){
         try {
-            FileInputStream file = new FileInputStream(path);
-            String str1 = "";
-            int index = 0;
-            while ((index = file.read()) != -1) {
-                str1 += (char) index;
+            FileInputStream fileInputStream = new FileInputStream(path);
+            String result="";
+            int i = 0;
+            while((i=fileInputStream.read())!=-1){
+                result+=(char)i;
             }
-            file.close();
-            return str1;
+            fileInputStream.close();
+            return result;
         }
-        catch (Exception e)
-        {
+        catch(Exception e){
             e.printStackTrace();
         }
         return null;
-    }
+    }//end
+
     /**
      * phương thức ghi một xâu ký tự mới vào file, nếu file đó đã có nội dung thì ta xóa nội dung cũ đi vào thêm nội dung mới vào
      * @param path  đường dẫn tới file cần ghi nội dung
      * @param newContent  Nội dung muốn ghi vào file
      */
-    public static void writeContentToFile(String path, String newContent) throws IOException{
-        FileOutputStream fileOutput = new FileOutputStream(path);
-        byte[] temp = newContent.getBytes();
-        fileOutput.write(temp);
-        fileOutput.close();
-    }
+    public static void writeContentToFile(String path, String newContent){
+        try{
+            FileOutputStream fileOutputStream = new FileOutputStream(path);
+            byte[] temp = newContent.getBytes();
+            fileOutputStream.write(temp);
+            fileOutputStream.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }//end
+
     /**
      * phương thức ghi vào thêm vào file nội dung mới mà không xóa đi nội dung cũ
      * @param path  đường dẫn tới file cần ghi
      * @param newContent  một xâu kí tự chứa nội dung mới cần ghi
      */
-    public static void addNewContentToFile(String path,String newContent){
-        try {
-            FileWriter fileWriter = new FileWriter(path, true);
+    public static void appendNewContentToFile(String path,String newContent){
+        try{
+            FileWriter fileWriter = new FileWriter(path,true);
             fileWriter.write("\n");
             fileWriter.write(newContent);
             fileWriter.close();
+
         }
-        catch (Exception e)
-        {
+        catch(Exception e){
             e.printStackTrace();
         }
-    }
+    }//end
+
     /**
      * phương thức tìm kiếm một file trong một thư mục
      * @param folderPath  đường dẫn của thư mục chưa file
      * @param fileName  tên của file cần tìm kiếm
      * @return  file cần tìm kiếm nếu có tồn tại hoăc null nếu nó không tồn tại
      */
-    public static File findFileByName(String folderPath, String fileName)
-    {
-        File temp = new File(folderPath);
-        String[] children = temp.list();
-        String file = null;
-        for (int i = 0; i < children.length; i++) {
-            if (children[i].equals(fileName)) file = folderPath;
+    public static File findFileByName(String folderPath, String fileName){
+        try{
+            File dic = new File(folderPath);
+            File[] arrFile = dic.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name.startsWith(fileName) && name.endsWith(".txt");
+                }
+            });
+            return arrFile[0];
         }
-        if (file == null) return new File("");
-        else {
-            file = file + "\\" + fileName;
-            File findedFile = new File(file);
-            return findedFile;
-        }
-    }
-
-    public static void main(String[] args) {
-        try {
-            String readFile = Utils.readContentFormFile("src/week9/Text.txt");
-            System.out.println(readFile);
-            String newaddContent = "Hi Hi Hi";
-            Utils.writeContentToFile("src/week9/Text.txt", newaddContent);
-            Utils.addNewContentToFile("src/week9/Text.txt","That's cú lừa!");
-            File searchFile = Utils.findFileByName("src/week9/","Text.txt");
-            if(searchFile!=null)    System.out.println("File có tồn tại");
-            else System.out.println("File không tồn tại");
-        }
-        catch (Exception e)
-        {
+        catch (Exception e){
             e.printStackTrace();
         }
-    }
+        return null;
+    }//end
+    public static void main(String args[]){
+        String readFormFile = Utils.readContentFormFile("src/week9/Text.txt");
+        System.out.println(readFormFile);
+        String newContent = "Hi World";
+        Utils.writeContentToFile("src/week9/Text.txt",newContent);
+        String newString = Utils.readContentFormFile("src/week9/Text.txt");
+        System.out.println(newString);
+        Utils.appendNewContentToFile("src/week9/Text.txt","Hello");
+        File searchFile = Utils.findFileByName("src/week9/","Text.txt");
+        if(searchFile!=null)    System.out.println("File is exist");
+        else System.out.println("File is not exist");
+    }//end
 }
