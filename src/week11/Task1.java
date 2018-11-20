60f65b95a233eefe8bec98366964a4a4a08b9dbd
Hoàng <hoang9cnd@gmail.com>
2018-11-20 20:44:22 +0700

    update

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,18 @@
 package week11;
 
+import java.util.Arrays;
+
+/**
+ * chương trình sắp xếp cho các kiểu dữ liệu(trừ boolean);
+ */
 public class Task1 {
+    static <T> void SortArr(T[] a){
+        Arrays.sort(a);
+    }
+
+    public static void main(String[] args) {
+        String[] a = {"f", "e", "c", "d", "a","b"};
+        SortArr(a);
+        System.out.println("Result: " + Arrays.toString(a));
+    }
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,18 @@
 package week11;
-public class Task2 {
 
+import java.util.Arrays;
+
+/**
+ * chương trình tìm max của các kiểu dữ liệu(trừ boolean)
+ */
+public class Task2 {
+    static <T> T MaxOfArr(T[] a){
+        Arrays.sort(a);
+        return a[a.length- 1];
+    }
+    public static void main(String[] args) {
+        String[] a = {"f", "e", "c", "d", "a","b"};
+        String max = MaxOfArr(a);
+        System.out.println("Max: " + max);
+    }
 }
