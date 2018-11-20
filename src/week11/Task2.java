f694ac63203bd523584ed999efaa7bc9d76d764a
Hoàng <hoang9cnd@gmail.com>
2018-11-20 21:46:10 +0700

    update

--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,18 +1,30 @@
 package week11;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+
 
 /**
  * chương trình tìm max của các kiểu dữ liệu(trừ boolean)
  */
 public class Task2 {
-    static <T> T MaxOfArr(T[] a){
-        Arrays.sort(a);
-        return a[a.length- 1];
+    static <T extends Comparable> T MaxOfArr(ArrayList<T> mylist){
+        T max = mylist.get(0);
+        for(int i = 1; i < mylist.size(); i++)
+        {
+            if(mylist.get(i).compareTo(max) > 0) max = mylist.get(i);
+        }
+        return max;
     }
     public static void main(String[] args) {
-        String[] a = {"f", "e", "c", "d", "a","b"};
-        String max = MaxOfArr(a);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(new Integer(10));
+        list.add(new Integer(199));
+        list.add(new Integer(1232));
+        list.add(new Integer(112));
+        list.add(new Integer(103213));
+        list.add(new Integer(113190));
+
+        Integer max = MaxOfArr(list);
         System.out.println("Max: " + max);
     }
 }
